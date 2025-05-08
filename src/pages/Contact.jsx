01ac3ae3ebd34f2c70f4cc1ff42cc234b3c55ac1
Anastasiia Horbachova <77613323+Greenfield-Taster@users.noreply.github.com>
2025-05-08 15:18:16 +0300

    Fix contact structure and add animated man

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import useScrollAnimation from "../utils/useScrollAnimation";
 import workingManImage from "../assets/working-man.png";
+import UpImageBg from "../assets/contact-bg.png";
 import partnersData from "../data/partners.json";
 import "../styles/pages/_Contact.scss";
 
@@ -13,49 +14,23 @@ const Contact = () => {
   return (
     <div className="contact-page">
       <div className="contact-banner">
-        <img src={workingManImage} alt={t("contact.workingManAlt")} />
+        <img
+          className="background-image"
+          src={UpImageBg}
+          alt={t("contact.backgroundAlt")}
+        />
+
+        <img
+          className="working-man-image"
+          src={workingManImage}
+          alt={t("contact.workingManAlt")}
+        />
       </div>
 
       <div className="contact-container">
-        <div className="contact-header">
-          <h1>{t("contact.title")}</h1>
-        </div>
-
-        <div className="contact-content">
-          <div className="contact-card">
-            <div className="contact-card-inner">
-              <h2>{t("contact.phones")}</h2>
-              <p>+380986032592</p>
-            </div>
-          </div>
-
-          <div className="contact-card">
-            <div className="contact-card-inner">
-              <h2>{t("contact.officeAddress")}</h2>
-              <p>
-                69035, {t("contact.zaporizhzhiaRegion")},{" "}
-                {t("contact.zaporizhzhiaCity")},{" "}
-                {t("contact.independenceStreet")},{t("contact.building")} 68А,{" "}
-                {t("contact.office")} 26
-              </p>
-              <p>{t("contact.email")}: info@th-capitalmarket.com</p>
-            </div>
-          </div>
-
-          <div className="contact-card">
-            <div className="contact-card-inner">
-              <h2>{t("contact.workingHours")}</h2>
-              <p>{t("contact.mondayToFriday")}: 9:00 - 18:00</p>
-              <p>
-                {t("contact.saturdaySunday")}: {t("contact.weekend")}
-              </p>
-            </div>
-          </div>
-        </div>
-
         <div className="partners-section">
           <div className="partners-header">
-            <h2>{t("contact.ourPartners")}</h2>
+            <h1>{t("contact.ourPartners")}</h1>
           </div>
           <div className="partners-grid">
             {partnersData.map((partner) => (
@@ -72,6 +47,39 @@ const Contact = () => {
                 />
               </a>
             ))}
+          </div>
+        </div>
+
+        <div className="contact-header">
+          <h1>{t("contact.title")}</h1>
+        </div>
+
+        <div className="contact-content">
+          <div className="contact-card">
+            <div className="contact-card-inner">
+              <p>+380986032592</p>
+              <p> info@th-capitalmarket.com</p>
+            </div>
+          </div>
+
+          <div className="contact-card">
+            <div className="contact-card-inner">
+              <p>
+                69035, {t("contact.zaporizhzhiaRegion")},
+                {t("contact.zaporizhzhiaCity")},
+                {t("contact.independenceStreet")},{t("contact.building")} 68А,
+                {t("contact.office")} 26
+              </p>
+            </div>
+          </div>
+
+          <div className="contact-card">
+            <div className="contact-card-inner">
+              <p>{t("contact.mondayToFriday")}: 9:00 - 18:00</p>
+              <p>
+                {t("contact.saturdaySunday")}: {t("contact.weekend")}
+              </p>
+            </div>
           </div>
         </div>
       </div>
