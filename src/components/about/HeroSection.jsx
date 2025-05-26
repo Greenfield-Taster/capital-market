38385429c16e5fadf01d59f5dee0f2dbae541b5a
Anastasiia Horbachova <77613323+Greenfield-Taster@users.noreply.github.com>
2025-05-26 10:21:53 +0300

    Add start animation to the hero section

--- src/components/about/HeroSection.jsx
+++ src/components/about/HeroSection.jsx
@@ -1,15 +1,25 @@
-import React from "react";
-import { Eye, Sparkles } from "lucide-react";
+import { useEffect, useState } from "react";
+import { Eye } from "lucide-react";
 import aboutPhoto from "../../assets/about-us/about-1.jpg";
 import "../../styles/components/about/HeroSection.scss";
 import { Link } from "react-router-dom";
 
 const HeroSection = ({ data }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(true);
+    }, 200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <section id="hero" className="hero-section">
       <div className="container">
         <div className="hero-content">
-          <div className="hero-text">
+          <div className={`hero-text ${isVisible ? "animate-in" : ""}`}>
             <h1 className="hero-title">
               <span className="gradient-text">{data.title}</span>
               <div className="title-decoration"></div>
@@ -29,8 +39,8 @@ const HeroSection = ({ data }) => {
             </div>
           </div>
 
-          <div className="hero-image">
-            <div className="image-placeholder-hero ">
+          <div className={`hero-image ${isVisible ? "animate-in" : ""}`}>
+            <div className="image-placeholder-hero">
               <img src={aboutPhoto} alt="aboutPhoto" />
               <div className="image-glow"></div>
             </div>
